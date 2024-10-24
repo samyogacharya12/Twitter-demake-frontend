73d9ec4cf9b0e707ecf988c337a96473be6f8e52
gauravkhadka40140 <gauravkhadka97@gmail.com>
2024-10-25 00:01:33 +0545

    profile design fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,14 @@ import {LoginComponent} from "./login/login.component";
 import {AppComponent} from "./app.component";
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {path: 'registration', component: RegistrationComponent},
-  { path: 'dashboard', component: DashboardComponent},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard/profile', component: ProfileComponent },
+  
   { path: '**', redirectTo: 'login' }
 ];
 @NgModule({
