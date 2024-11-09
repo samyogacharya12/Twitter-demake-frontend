c3dd902fa4f2104d97a838bb55d771ea13c0654c
gauravkhadka40140 <gauravkhadka97@gmail.com>
2024-11-09 22:43:07 +0545

    design-fixes

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,24 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
-
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent {
-   isModalVisible = false;
+  isModalVisible = false;
+  isCardBoxVisible = false;
+  @ViewChild('cardBox') cardBox: ElementRef | undefined;
 
-  constructor(private router: Router,private location:Location) {
-    
+  constructor(private router: Router, private location: Location) {}
+
+  goBack(): void {
+    this.location.back(); // This will navigate to the previous page
   }
-
-   goBack(): void {
-    this.location.back();  // This will navigate to the previous page
-  }
-
 
   public scrollLeft() {
     const container = document.querySelector('.overflow-x-auto') as HTMLElement;
@@ -30,13 +28,29 @@ export class ProfileComponent {
     container.scrollBy({ left: 250, behavior: 'smooth' }); // Adjust scroll distance as needed
   }
 
- 
-
   public openModal() {
     this.isModalVisible = true;
   }
 
   public closeModal() {
     this.isModalVisible = false;
+  }
+
+  toggleCard(event: Event) {
+    event.stopPropagation(); // Prevent click propagation to document
+    this.isCardBoxVisible = !this.isCardBoxVisible;
+  }
+
+  // Close the card box if clicked outside of it
+  @HostListener('document:click', ['$event'])
+  closeCardBox(event: MouseEvent) {
+    if (this.cardBox && !this.cardBox.nativeElement.contains(event.target)) {
+      this.isCardBoxVisible = false; // Close card box if clicked outside
+    }
+  }
+
+  onOptionClick(event: MouseEvent) {
+    event.stopPropagation(); // Prevent click from closing the card-box
+    console.log('Option clicked:', event.target); // You can handle option selection here
   }
 }
